0199223987e32b0d06469677060b54cca6ee432f
Johan Brinkenberg <johan.brinkenberg@elev.ga.lbs.se>
2018-09-26 15:34:38 +0200

    ÄNDRADE TMPRO
    +CostText
    +DescriptionText

---
+++ Assets/CostCheck.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CostCheck : MonoBehaviour
+{ 
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (MouseFollow.mouseCheck != null)
+        {
+            if (MouseFollow.mouseCheck.cost > 0)
+                GetComponent<TextMeshPro>().SetText("$ " + MouseFollow.mouseCheck.cost.ToString());
+                
+        }
+
+    }
+}
---
+++ Assets/DescCheck.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DescCheck : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (MouseFollow.mouseCheck != null)
+        {
+                GetComponent<TextMeshPro>().SetText(MouseFollow.mouseCheck.description);
+
+        }
+
+    }
+
+}
